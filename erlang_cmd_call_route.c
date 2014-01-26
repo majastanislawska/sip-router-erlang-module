56f026dd403c11555eacad8b33ca476197201b6b
Grzegorz Stanislawski <stangrze@yahoo.com>
2014-01-26 02:17:56 +0100

    reuse common code

--- erlang_cmd_call_route.c
+++ erlang_cmd_call_route.c
@@ -36,11 +36,8 @@ int cmd_erlang_call_route(struct sip_msg* msg, char *cn , char *rp, char *ar, ch
 	static char printbuf[AVP_PRINTBUF_SIZE];
 	static char routename[MAXATOMLEN];
 	int printbuf_len, bytessent, i, j;
-	ei_x_buff argbuf;
+	ei_x_buff argbuf,retbuf;
 	struct nodes_list* node;
-	struct erlang_cmd *erl_cmd;
-	erlang_pid erl_pid;
-	erlang_ref ref;
 	pv_spec_t * ret_pv;
 	str conname, regproc;
 	struct run_act_ctx ra_ctx;
@@ -74,95 +71,30 @@ int cmd_erlang_call_route(struct sip_msg* msg, char *cn , char *rp, char *ar, ch
 		return -1;
 	}
 
-	erl_cmd=shm_malloc(sizeof(struct erlang_cmd));
-	if(!erl_cmd) {
-	    LM_ERR("no shm");
-	    return -1;
-	}
-	memset(erl_cmd,0,sizeof(struct erlang_cmd));
-
-	erl_cmd->erlbuf=shm_malloc(AVP_PRINTBUF_SIZE);
-	if (!erl_cmd->erlbuf) {
-	    LM_ERR("no shm memory\n\n");
-	    goto error;
-	}
-
 	ret_pv = (pv_spec_t*)shm_malloc(sizeof(pv_spec_t));
 	if (!ret_pv) {
 		LM_ERR("no shm memory\n\n");
 		return -1;
-	}
-
-	if(lock_init(&(erl_cmd->lock))==NULL) {
-	    LM_ERR("cannot init the lock\n");
-	    goto error;
-	}
-	erl_cmd->reg_name=shm_strdup(&regproc);
-	if (!erl_cmd->reg_name) {
-	    LM_ERR("no shm memory\n\n");
-	    goto error;
 	}
 	memcpy(ret_pv, (pv_spec_t *)_ret_pv, sizeof(pv_spec_t));
 
 	LM_DBG("cmd_erlang_call_route:  %.*s %.*s %.*s\n",conname.len,conname.s,
 			regproc.len,regproc.s, printbuf_len,printbuf);
 
-	erl_cmd->cmd=ERLANG_CALL;
-	erl_cmd->node=node;
-	argbuf.buff=erl_cmd->erlbuf;
-	argbuf.buffsz=AVP_PRINTBUF_SIZE-1;
-	argbuf.index=0;
-	ei_x_encode_version(&argbuf);
-	// gen_call is 3-element tuple, first is atom $gen_call, 
-	// second is {pid,ref} tuple and third is user data 
-	ei_x_encode_tuple_header(&argbuf, 3);
-	ei_x_encode_atom(&argbuf, "$gen_call");
-	ei_x_encode_tuple_header(&argbuf, 2);
-	// we are hacking erlang pid so we can have worker system pid here
-	memcpy(&erl_pid,ei_self(&(node->ec)),sizeof(erlang_pid));
-//	erl_pid.num=getpid();
-	ei_x_encode_pid(&argbuf, &erl_pid);
-	utils_mk_ref(&(node->ec),&ref);
-	ei_x_encode_ref(&argbuf, &ref);
-	erl_cmd->refn0=ref.n[0];
-	erl_cmd->refn1=ref.n[1];
-	erl_cmd->refn2=ref.n[2];
-
-	//so much for pid, now encode rex call tuple {call, mod, fun, arg, user}
-//	ei_x_encode_tuple_header(&argbuf, 5);
-//	ei_x_encode_atom(&argbuf,"call");
-//	ei_x_encode_atom_len(&argbuf, mod.s, mod.len);
-//	ei_x_encode_atom_len(&argbuf, fun.s, fun.len);
+	ei_x_new(&argbuf);
 	if(ei_x_format_wo_ver(&argbuf, printbuf)!=0) {
 		LM_ERR("cannot fromat erlang binary from arg string\n");
 		goto error;
 	}
-//	ei_x_encode_atom(&argbuf,"user");
-	erl_cmd->erlbuf_len=argbuf.index;
-
-	lock_get(&(erl_cmd->lock));
-	bytessent=write(pipe_fds[1], &erl_cmd, sizeof(erl_cmd));
-	LM_DBG("cmd_erlang_call: locked, sent %d  %d %s %d %p %p, waiting for release\n",bytessent,
-			erl_cmd->cmd,erl_cmd->reg_name,erl_cmd->erlbuf_len,erl_cmd->erlbuf, erl_cmd);
-	lock_get(&(erl_cmd->lock));
-	LM_DBG("after lock\n");
-	if (erl_cmd->retcode <0) {
-	    retcode=erl_cmd->retcode;
-	    LM_DBG("cmd_erlang_call: failed %d\n",retcode);
-	    goto error;
-	}
-	//reuse
-	argbuf.buff=erl_cmd->erlbuf;
-	argbuf.buffsz=erl_cmd->erlbuf_len;
-	argbuf.index=erl_cmd->decode_index;
-
+	do_erlang_call(&conname, &regproc, &argbuf, &retbuf);
 	//we have a two element tuple here
-	ei_decode_tuple_header(argbuf.buff, &(argbuf.index), &i);
+	ei_decode_tuple_header(retbuf.buff, &(retbuf.index), &i);
 	//first element should be atom we can add check here
-	ei_get_type(argbuf.buff, &(argbuf.index), &i, &j);
-	ei_decode_atom(argbuf.buff, &(argbuf.index), routename);
+	ei_get_type(retbuf.buff, &(retbuf.index), &i, &j);
+	ei_decode_atom(retbuf.buff, &(retbuf.index), routename);
 	//second element is put to ret_pv wihout bothering what is it
-	fill_retpv(erl_cmd->ret_pv,&argbuf,&(argbuf.index));
+	fill_retpv(ret_pv,&retbuf,&(retbuf.index));
+	if(retbuf.buff) shm_free(retbuf.buff);
 
 	//execute route
 	route_no=route_get(&main_rt, routename);
@@ -181,11 +113,7 @@ int cmd_erlang_call_route(struct sip_msg* msg, char *cn , char *rp, char *ar, ch
 	}
 	retcode=(call_route_exit)?0:1;
 error:
-	if(erl_cmd) {
-	    if(erl_cmd->ret_pv) shm_free(erl_cmd->ret_pv);
-	    if(erl_cmd->erlbuf) shm_free(erl_cmd->erlbuf);
-	    if(erl_cmd->reg_name) shm_free(erl_cmd->reg_name);
-	    shm_free(erl_cmd);
-	}
+
+	ei_x_free(&argbuf);
 	return retcode;
 }
